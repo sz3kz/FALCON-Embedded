7c386c078fb078cb790a66844c1bd8a8d2066237
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-04-08 18:38:28 +0200

    Add MPU detection

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@
 
 setup:
 	arduino-cli core update-index && arduino-cli core install arduino:avr
+	arduino-cli lib install 'Adafruit MPU6050' 'Adafruit Unified Sensor' 'Adafruit BusIO'
 
 build:
 	arduino-cli compile --fqbn arduino:avr:uno --build-path ./build MyProject
--- MyProject/MyProject.ino
+++ MyProject/MyProject.ino
@@ -1,8 +1,12 @@
 //#include <Arduino.h>
 //#include <Wire.h>
-//
+#include <Adafruit_MPU6050.h>
+//#include <Adafruit_Sensor.h>
+
 const int MY_OUTPUT_PIN = LED_BUILTIN;
 const int MY_INTERRUPT_PIN = 2;
+const int SERIAL_THROUGHPUT = 9600;
+Adafruit_MPU6050 mpu;
 
 // can change behind compiler's back - don't out-optimize 
 volatile bool buttonPressed = false;
@@ -13,11 +17,20 @@ void buttonEvent() {
 }
 
 void setup() {
+  Serial.begin(SERIAL_THROUGHPUT);
   pinMode(MY_OUTPUT_PIN, OUTPUT);
   pinMode(MY_INTERRUPT_PIN, INPUT_PULLUP);
 
   // Register interrupt
   attachInterrupt(digitalPinToInterrupt(MY_INTERRUPT_PIN), buttonEvent, CHANGE);
+
+  // Detect MPU
+  if (! mpu.begin()){
+    Serial.println("MPU not detected!");
+    while (1)
+      delay(1000);
+  }
+  Serial.println("MPU detected!");
 }
 
 void loop() {
